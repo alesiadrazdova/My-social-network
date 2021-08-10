f1ea43ef397d0c1cb540c52c126a7f6a1aa70fbc
Alesia Drazdova <alesiadrozdova@gmail.com>
2021-08-10 13:20:20 +0300

    improved code

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
+const baseURL = 'https://social-network.samuraijs.com/api/1.0/';
 
 export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 10) {
-        return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {
+        return axios.get(`${baseURL}users?page=${currentPage}&count=${pageSize}`, {
             withCredentials: true
         }).then((response) => {
-            return response.data
+            return response.data;
         });
     }
-}
+};
